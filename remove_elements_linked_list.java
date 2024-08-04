c6a2e2836047e2670432e401c31633615429d7da
Balasubramanian <balavan2005@gmail.com>
2024-08-04 22:55:07 +0530

    day7_2

--- remove_elements_linked_list.java
+++ remove_elements_linked_list.java
@@ -1,3 +1,26 @@
-public class remove_elements_linked_list {
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = head;
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
 }
